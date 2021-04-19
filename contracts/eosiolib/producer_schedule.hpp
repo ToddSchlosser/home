d09375a7656b86a9147c511a5b1bb7fc30664404
ToddSchlosser <35857526+ToddSchlosser@users.noreply.github.com>
2021-04-19 16:05:44 -0400

    Initialize system-test-fix-draft

---
+++ contracts/eosiolib/producer_schedule.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <eosiolib/privileged.hpp>
+
+#include <vector>
+
+namespace eosio {
+   /**
+    *  Defines both the order, account name, and signing keys of the active set of producers.
+    */
+   struct producer_schedule {
+      uint32_t                     version;   ///< sequentially incrementing version number
+      std::vector<producer_key>    producers;
+
+      EOSLIB_SERIALIZE( producer_schedule, (version)(producers) )
+   };
+} /// namespace eosio
