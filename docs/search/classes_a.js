ca483c4880a040447fb50b6a659275c378ff8bbc
ToddSchlosser <35857526+ToddSchlosser@users.noreply.github.com>
2021-04-19 16:04:36 -0400

    Initialize contextfree

---
+++ docs/search/classes_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['name',['name',['../structeosio_1_1name.html',1,'eosio']]],
+  ['native_5fcurrency',['native_currency',['../structeosio_1_1native__currency.html',1,'eosio']]]
+];
