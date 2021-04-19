032f46ca6f665e803d7cd6ca61144d7b18852921
ToddSchlosser <35857526+ToddSchlosser@users.noreply.github.com>
2021-04-19 16:10:05 -0400

    Initialize rodeos_pre_squash_the_second

---
+++ libraries/chain/include/eosio/chain/webassembly/runtime_interface.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <memory>
+
+namespace eosio { namespace chain {
+
+class apply_context;
+
+class wasm_instantiated_module_interface {
+   public:
+      virtual void apply(apply_context& context) = 0;
+
+      virtual ~wasm_instantiated_module_interface();
+};
+
+class wasm_runtime_interface {
+   public:
+      virtual std::unique_ptr<wasm_instantiated_module_interface> instantiate_module(std::vector<uint8_t>&& wasm, std::vector<uint8_t>&& initial_memory,
+                                                                                     const digest_type& code_hash, const uint8_t& vm_type, const uint8_t& vm_version) = 0;
+
+      //immediately exit the currently running wasm_instantiated_module_interface. Yep, this assumes only one can possibly run at a time.
+      virtual void immediately_exit_currently_running_module() = 0;
+
+      virtual ~wasm_runtime_interface();
+};
+
+}}
