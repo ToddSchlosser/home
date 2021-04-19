61af84a55719e9029c3904b1171b459307f6987f
ToddSchlosser <35857526+ToddSchlosser@users.noreply.github.com>
2021-04-19 16:10:23 -0400

    Initialize more-blocklog-integration-test-info

---
+++ libraries/chain/block_header.cpp
@@ -0,0 +1,89 @@
+#include <eosio/chain/block.hpp>
+#include <eosio/chain/merkle.hpp>
+#include <fc/io/raw.hpp>
+#include <fc/bitutil.hpp>
+#include <algorithm>
+
+namespace eosio { namespace chain {
+   digest_type block_header::digest()const
+   {
+      return digest_type::hash(*this);
+   }
+
+   uint32_t block_header::num_from_id(const block_id_type& id)
+   {
+      return fc::endian_reverse_u32(id._hash[0]);
+   }
+
+   block_id_type block_header::id()const
+   {
+      // Do not include signed_block_header attributes in id, specifically exclude producer_signature.
+      block_id_type result = digest(); //fc::sha256::hash(*static_cast<const block_header*>(this));
+      result._hash[0] &= 0xffffffff00000000;
+      result._hash[0] += fc::endian_reverse_u32(block_num()); // store the block num in the ID, 160 bits is plenty for the hash
+      return result;
+   }
+
+   flat_multimap<uint16_t, block_header_extension> block_header::validate_and_extract_header_extensions()const {
+      using decompose_t = block_header_extension_types::decompose_t;
+
+      flat_multimap<uint16_t, block_header_extension> results;
+
+      uint16_t id_type_lower_bound = 0;
+
+      for( size_t i = 0; i < header_extensions.size(); ++i ) {
+         const auto& e = header_extensions[i];
+         auto id = e.first;
+
+         EOS_ASSERT( id >= id_type_lower_bound, invalid_block_header_extension,
+                     "Block header extensions are not in the correct order (ascending id types required)"
+         );
+
+         auto iter = results.emplace(std::piecewise_construct,
+            std::forward_as_tuple(id),
+            std::forward_as_tuple()
+         );
+
+         auto match = decompose_t::extract<block_header_extension>( id, e.second, iter->second );
+         EOS_ASSERT( match, invalid_block_header_extension,
+                     "Block header extension with id type ${id} is not supported",
+                     ("id", id)
+         );
+
+         if( match->enforce_unique ) {
+            EOS_ASSERT( i == 0 || id > id_type_lower_bound, invalid_block_header_extension,
+                        "Block header extension with id type ${id} is not allowed to repeat",
+                        ("id", id)
+            );
+         }
+
+
+         id_type_lower_bound = id;
+      }
+
+      return results;
+   }
+
+   fc::optional<producer_schedule_change_extension> block_header::get_new_producer_schedule(const flat_multimap<uint16_t, block_header_extension>& exts) {
+      //ilog("REMOVE  get_new_producer_schedule");
+      if ( exts.count(producer_schedule_change_extension::extension_id()) > 0 ) {
+         //ilog("REMOVE  get_new_producer_schedule 1");
+         return exts.lower_bound(producer_schedule_change_extension::extension_id())->second.get<producer_schedule_change_extension>();
+      }
+      //ilog("REMOVE  get_new_producer_schedule 2");
+
+      return {};
+   }
+
+   fc::optional<protocol_feature_activation> block_header::get_new_protocol_feature_activation(const flat_multimap<uint16_t, block_header_extension>& exts) {
+      //ilog("REMOVE  get_new_protocol_feature_activation");
+      if( exts.count(protocol_feature_activation::extension_id()) > 0 ) {
+         //ilog("REMOVE  get_new_protocol_feature_activation 1");
+         return exts.lower_bound(protocol_feature_activation::extension_id())->second.get<protocol_feature_activation>().protocol_features;
+      }
+      //ilog("REMOVE  get_new_protocol_feature_activation 2");
+
+      return {};
+   }
+
+} }
